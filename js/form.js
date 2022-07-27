dddfb3322ba86b81222d29d57b89b8e1b169bb5e
jodixn1 <94428120+jodixn1@users.noreply.github.com>
2022-07-27 12:12:42 -0700

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -1,23 +1,19 @@
-var elForm, elSelectPackage, elPackageHint, elTerms, elTermsHint; // Declare variables
+var elForm, elSelectPackage, elPackageHint, elTerms, elTermsHint;elusername; elpassword // Declare variables
 elForm          = document.getElementById('formSignup');          // Store elements
 elSelectPackage = document.getElementById('package');
 elPackageHint   = document.getElementById('packageHint');
-elTerms         = document.getElementById('terms');
-elTermsHint     = document.getElementById('termsHint');
-
+// elTerms         = document.getElementById('terms');
+// elTermsHint     = document.getElementById('termsHint');
+elTerms         = document.getElementById('username');
+elTermsHint     = document.getElementById('usernameHint');
+elTerms         = document.getElementById('password');
+elTermsHint     = document.getElementById('passwordHint');
 function packageHint() {                                 // Declare function
   var pack = this.options[this.selectedIndex].value;     // Get selected option
-  if (pack === 'monthly') {                              // If monthly package
-    elPackageHint.innerHTML = 'Save $10 if you pay for 1 year!';//Show this msg
+  if (pack === 'Username') {                              // If username package
+    elPackageHint.innerHTML = 'Thanks you will recive info on new classes.';//Show this msg
   } else {                                               // Otherwise
-    elPackageHint.innerHTML = 'Wise choice!';            // Show this message
-  }
-}
-
-function checkTerms(event) {                             // Declare function
-  if (!elTerms.checked) {                                // If checkbox ticked
-    elTermsHint.innerHTML = 'You must agree to the terms.'; // Show message
-    event.preventDefault();                              // Don't submit form
+    elPackageHint.innerHTML = 'Thank you!';            // Show this message
   }
 }
 
---
+++ js/signup.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>JavaScript &amp; jQuery - Chapter 6: Events - Event Listener and Event Object</title>
+    <link rel="stylesheet" href="css/c06.css" />
+  </head>
+  <body>
+    <div id="page">
+           <form method="post" action="http://www.example.org/register">
+
+        <label for="username">username: </label>
+        <input type="text" id="username" /><div id="feedback"></div>password: </label>
+        <input type="password" id="password" /><div id="feedback"></div>
+
+        <input type="submit" value="sign up" />
+
+      </form>
+    </div>
+    <script src="js/event-listener-with-event-object.js"></script>
+  </body>
+</html>
