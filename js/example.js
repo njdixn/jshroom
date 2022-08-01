4c2fe09dbd37edd2ae3bf0cbc0648a4730493a6c
jodixn1 <94428120+jodixn1@users.noreply.github.com>
2022-07-31 20:23:34 -0700

    Add files via upload

--- js/example.js
+++ js/example.js
@@ -14,25 +14,25 @@ $(function() {                                    // When the DOM is ready
   });
 
   // FUNCTION THAT COLLECTS DATA FROM THE JSON FILE
-  function loadTimetable() {                    // Declare function
+  function loadClasses() {                    // Declare function
     $.getJSON('data/example.json')              // Try to collect JSON data
     .done( function(data){                      // If successful
       times = data;                             // Store it in a variable
     }).fail( function() {                       // If a problem: show message
-      $('#event').html('Sorry! We could not load the timetable at the moment');
+      $('#event').html('Sorry! We could not load the classes at the moment');
     });
   }
 
-  loadTimetable();                              // Call the function
+  loadClasses();                              // Call the function
 
 
-  // CLICK ON THE EVENT TO LOAD A TIMETABLE 
+  // CLICK ON THE EVENT TO LOAD the classes 
   $('#content').on('click', '#event a', function(e) {  // User clicks on event
 
     e.preventDefault();                                // Prevent loading page
     var loc = this.id.toUpperCase();                   // Get value of id attr
 
-    var newContent = '';                               // Build up timetable by
+    var newContent = '';                               // Build up classes by
     for (var i = 0; i < times[loc].length; i++) {      // looping through events
       newContent += '<li><span class="time">' + times[loc][i].time + '</span>';
       newContent += '<a href="descriptions.html#';
