aa3c17c14a4fc395511c8d7d0345e3be455b26d9
jodixn1 <94428120+jodixn1@users.noreply.github.com>
2022-08-02 10:37:33 -0700

    w
    w

--- data/data-jsonp.js
+++ data/data-jsonp.js
@@ -1,16 +1,16 @@
-showEvents({
-  "events": [
-    {
-      "classes": "Sewing",
-      "pic": "images/sewing-sm.jpg"
-    },
-    {
-      "classes": "Resin",
-      "pic": "images/resin-sm.jpg"
-    },
-    {
-      "classes": "Crocheting",
-      "pic": "images/crocheting-sm.jpg"
+function showEvents(data) {                           // Callback when JSON loads
+  var newContent = '';                                // Variable to hold HTML
+ 
+    // BUILD UP STRING WITH NEW CONTENT (could also use DOM manipulation)
+    for (var i = 0; i < data.events.length; i++) {    // Loop through object
+      newContent += '<div class="event">';
+      newContent += '<img src="' + data.events[i].map + '" ';
+      newContent += 'alt="' + data.events[i].location + '" />';
+      newContent += '<p><b>' + data.events[i].location + '</b><br>';
+      newContent += data.events[i].date + '</p>';
+      newContent += '</div>';
     }
-  ]
-});
+
+    // Update the page with the new content
+    document.getElementById('content').innerHTML = newContent;
+}
