fef46dddfcfe5f2b2e9fcee324ad0eb8efac5d4b
jodixn1 <94428120+jodixn1@users.noreply.github.com>
2022-08-02 10:54:24 -0700

    2
    2

--- data/data-jsonp.js
+++ data/data-jsonp.js
@@ -2,14 +2,17 @@ showEvents({
   "events": [
     {
       "location": "Sewing",
+      "date": "Aug",
       "map": "images/sewing-sm.jpg"
     },
     {
       "location": "Resin",
+      "date": "Aug",
       "map": "images/resin-sm.jpg"
     },
     {
       "location": "Crocheting",
+      "date": "Aug",
       "map": "images/crocheting-sm.jpg"
     }
   ]
